1301975b8afc3c15b3099d446b0373b062ad648a
TiagoFollador <tiagobritofollador@gmail.com>
2024-10-24 20:31:44 -0300

    Registro Criado

--- Divisao/Controller.java
+++ Divisao/Controller.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class Controller {
+public class Controller {
+    private Hash hash;
+    private Registro registro;
+
+
+    public Controller(int tamanhoTabela) {
+        this.hash = new Hash(tamanhoTabela);
+        this.registro = new Registro(tamanhoTabela);
+    }
+
+    private Elemento criarElemento(int valor)
+    {
+        int chave = hash.funcaoHash(valor);
+        Elemento e = new Elemento(chave, valor);
+        return e;
+    }
+
+    private boolean inserirRegistro(Elemento e)
+    {
+        return this.registro.inserirRegistro(e);
+    }
+
+//    public boolean popularRegistro()
+
+
 }
--- Divisao/Registro.java
+++ Divisao/Registro.java
@@ -1,4 +1,49 @@
 public class Registro
 {
-    private int tamanhoTabela;
+    private Elemento[] registroElementos;
+    private int tamanhoAtual;
+    private int tamanhoMaximo;
+
+    public Registro(int tamanhoLista)
+    {
+        this.registroElementos = new Elemento[tamanhoLista];
+        this.tamanhoAtual = 0;
+        this.tamanhoMaximo = tamanhoLista - 1;
+    }
+
+    public boolean inserirRegistro(Elemento elemento)
+    {
+        if (tamanhoAtual < tamanhoMaximo) {
+            if (encontrarChavesIguais(elemento)) {
+                return true;
+            } else {
+                this.registroElementos[this.tamanhoAtual] = elemento;
+                tamanhoAtual++;
+                return true;
+            }
+        } else {
+            return false;
+        }
+    }
+
+    private boolean encontrarChavesIguais(Elemento elemento)
+    {
+        for (int i = 0; i < tamanhoAtual; i++) {
+            if (registroElementos[i].getChave() == elemento.getChave()) {
+                Elemento lastElemento = findLastElemento(registroElementos[i]);
+                lastElemento.setProximo(elemento);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private Elemento findLastElemento(Elemento e)
+    {
+        if (e.getProximo() == null) {
+            return e;
+        } else {
+            return findLastElemento(e.getProximo());
+        }
+    }
 }
